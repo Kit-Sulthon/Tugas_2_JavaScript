c8089effe30d6f5690134952f3d3510fc5ef3efd
T R A $ H <sulthons.a19@gmail.com>
2023-10-03 11:01:05 +0700

    Tugas 2 JavaScript
    Tugas ini kami kumpulkan untuk Niomic

---
+++ tugas2.js
@@ -0,0 +1,21 @@
+var Nama = "Sulthon Syarif";
+Nama = null;
+// Null [1]
+// 
+var asal = '/ surabaya';
+// String[2]
+// 
+var tinggibadan = false; 
+var cm = 168;
+if (cm>170)
+tinggibadan = true;
+else
+tinggibadan = false;
+// Boolean[3]
+// Array [4]
+var saudara = [2,4,6,7,9];
+//
+// Undifined [5]
+var Girlfriend;
+//
+console.log(Nama, asal, tinggibadan,saudara[1], Girlfriend);
